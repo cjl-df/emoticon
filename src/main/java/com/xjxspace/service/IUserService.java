bdd484e98644386dfc9c041b7686cf749884b39b
sadamu0912 <1147644413@qq.com>
2019-08-05 00:21:05 +0800

    '还没完成，添加阿里云短信'

--- src/main/java/com/xjxspace/service/IUserService.java
+++ src/main/java/com/xjxspace/service/IUserService.java
@@ -6,4 +6,10 @@ import com.xjxspace.model.system.User;
 public interface IUserService {
 
     User findByUserAccount(String userAccout);
+
+    User findUserByTelephone(String telephone);
+
+    User addUserByPhone(String telephone);
+
+
 }
--- src/main/java/com/xjxspace/service/impl/UserServiceImpl.java
+++ src/main/java/com/xjxspace/service/impl/UserServiceImpl.java
@@ -1,13 +1,14 @@
-package com.xjxspace.service;
+package com.xjxspace.service.impl;
 
 
 import com.xjxspace.mapper.system.UserMapper;
 import com.xjxspace.model.system.User;
+import com.xjxspace.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements IUserService{
+public class UserServiceImpl implements IUserService {
 
     @Autowired
     private UserMapper userMapper;
